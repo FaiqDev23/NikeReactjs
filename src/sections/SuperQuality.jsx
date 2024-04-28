4bf8e7f544117f76ba2e4bffa8fc6bb4c1b64b2b
FaiqDev23 <faiqrustam178@gmail.com>
2024-04-28 13:38:45 +0400

    quality section finished

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,24 +1,42 @@
 import Button from "../components/Button"
+import { shoe8 } from "../assets/images"
 const SuperQuality = () => {
   return (
-    <section 
-    id="about-us"
-    className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    <section
+      id='about-us'
+      className='flex justify-between items-center max-lg:flex-col gap-10 w-full max-container'
     >
+      <div className='flex flex-1 flex-col'>
+        <h2 className='font-palanquin capitalize text-4xl lg:max-w-lg font-bold'>
+          We Provide You
+          <span className='text-coral-red'> Super </span>
+          <span className='text-coral-red'>Quality </span> Shoes
+        </h2>
+        <p className='mt-4 lg:max-w-lg info-text'>
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+        </p>
+        <p className='mt-6 lg:max-w-lg info-text'>
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+        <div className='mt-11'>
+          <Button label='View details' />
+        </div>
+      </div>
 
-      <div>
-      <p className="text-xl font-montserrat text-coral-red">Our Summer Collection</p>
-         <h1 className="mt-10 font-palanquin text-8xl max-sm:[72px] max-sm:leading-[82] font-bold">
-          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
-          <br />
-          <span className="text-coral-red inline-block mt-3 ">Nike</span>
-          Shoes
-          </h1>
-          <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish Nike arrivals, quality comfort,and innovation for your active life.</p>
-          <Button label="View details" />
+      <div className='flex-1 flex justify-center items-center'>
+        <img
+          src={shoe8}
+          alt='product detail'
+          width={570}
+          height={522}
+          className='object-contain'
+        />
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default SuperQuality
+
